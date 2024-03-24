c1b72e45915616428bfa29fd2a09b1b11c287d49
Cristhian <Cristhian@DESKTOP-OCO54ET>
2024-03-23 23:49:41 -0400

    Add project files.

---
+++ Pantalla/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace TuProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
